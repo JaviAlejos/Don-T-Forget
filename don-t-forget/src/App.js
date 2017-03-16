40c19e3e65ce188de4a6187ce861a665e3f65894
Javi Alejos <javieralejos@MBP-de-Javier.home>
2017-03-16 23:50:56 +0100

    Styles and Logo

--- don-t-forget/src/App.js
+++ don-t-forget/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../public/Remember.png' ;
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Don't Forget</h1>
+          <img src={logo} height="90%" width="60%" alt="logo" />
         </div>
         <p className="App-intro">
-            <h2>The Web that remnind you all</h2>
+            <h2 className="App-fuente-intro">The Web that remnind you all</h2>
+            <span className="App-fuente">Do you usually forget birthdays?</span>
             <br/>
-            <span>Do you usually forget birthdays?</span>
+            <span className="App-fuente">Do you usually forget your passwords?</span>
             <br/>
-            <span>Do you usually forget your passwords?</span>
+            <span className="App-fuente">Do you usually forget buy something?</span>
             <br/>
-            <span>Do you usually forget buy something?</span>
-            <br/>
-            <h3>This is your Website!!!!</h3>
+            <h3 className="App-fuente-intro">This is your Website!!!!</h3>
 
 
         </p>
