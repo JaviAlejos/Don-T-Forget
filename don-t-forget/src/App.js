f293e1f8f6507f356442a820091aff1de563ba11
Javi Alejos <javieralejos@MBP-de-Javier.home>
2017-03-20 20:08:21 +0100

    Login and state with redux

--- don-t-forget/src/App.js
+++ don-t-forget/src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
 import AppCarousel from './AppCarousel';
 import AppNavBar from './AppNavBar';
+import { createStore } from 'redux';
+
+var dispatch=function (state, action) {
+debugger;
+  switch (action.type) {
+  case 'ADD_USER':
+    return {user:action.user};
+  default:
+    return state;
+  }
+}
+
+const store = createStore(dispatch,{
+    user: ""
+});
+
 
 class App extends Component {
-  render() {
+
+constructor(){
+  super();
+
+  this.state={
+      user: ""
+  }
+
+  store.subscribe(() =>
+  this.setState(store.getState()));
+console.log("aaaaa");
+  }
+
+renderCarousel(){
+
+if (!store.getState().user){
+return(
+  <AppCarousel/>
+);
+}else {
+  console.log("habria que borrar");
+  console.log(store.getState());
+}
+}
+
+render() {
     return (
 <div>
-      <AppNavBar/>
-      <AppCarousel/>
+      <AppNavBar store={store}/>
+      {this.renderCarousel()}
 </div>
 
     );
--- don-t-forget/src/AppNavBar.js
+++ don-t-forget/src/AppNavBar.js
@@ -1,32 +1,77 @@
-import React, { Component } from 'react';
-import { Button,Navbar,NavItem,Nav} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Button, Navbar, NavItem, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/App.css';
-import logo from '../public/Remember.png' ;
+import logo from '../public/Remember.png';
+import firebase from 'firebase';
 
 class AppNavBar extends Component {
-  render() {
-    return (
 
-      <div>
-        {/*<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
+    constructor() {
+        super();
+        this.logout=this.logout.bind(this);
+    }
+
+    login() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(result => console.log(`${result.user.email} ha iniciado sesión`)).catch(error => console.log(`error ${error.code}:${error.message}`));
+    }
+
+    logout() {
+        firebase.auth().signOut().then(result => console.log(`${result.user.email} ha cerrado sesión`)).catch(error => console.log(`${error.code}:${error.message}`));
+        this.props.store.dispatch({ type:'ADD_USER', user:""});
+        console.log("cierro sesion");
+
+    }
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(user => {
+          if (user!=null)
+            this.props.store.dispatch({ type:'ADD_USER', user});
+        });
+    }
+
+    renderButton() {
+        if (this.props.store.getState().user) {
+            return (
+                <Button bsSize="large" bsStyle="info" onClick={this.logout}>Sign Out</Button>
+            );
+
+        } else {
+            return (
+                <Button bsSize="large" bsStyle="info" onClick={this.login}>Sign In</Button>
+            );
+        }
+
+    }
+
+    render() {
+        return (
+
+            <div>
+                {/*<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css"/>*/}
-          {/* My NavBar */}
-        <Navbar className="App">
-            <Navbar.Header>
-              <img height="100%" width="70%" src={logo}  alt="logo" />
-            </Navbar.Header>
-            <Nav pullRight>
-              <NavItem><Button bsSize="large" bsStyle="info">Sign In</Button></NavItem>
-            </Nav>
-            <Nav className="App">
-              <NavItem> <h1 className="App"> Are you forgetting something?</h1></NavItem>
-            </Nav>
-        </Navbar>
-</div>
+                {/* My NavBar */}
+                <Navbar className="App">
+                    <Navbar.Header>
+                        <img height="100%" width="70%" src={logo} alt="logo"/>
+                    </Navbar.Header>
+                    <Nav pullRight>
+                        <NavItem>
+                            {this.renderButton()}
+                        </NavItem>
+                    </Nav>
+                    <Nav className="App">
+                        <NavItem>
+                            <h1 className="App">
+                                Are you forgetting something?</h1>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </div>
 
-);
-}
+        );
+    }
 }
 
 export default AppNavBar;
